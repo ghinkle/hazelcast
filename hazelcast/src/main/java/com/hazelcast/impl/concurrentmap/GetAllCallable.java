f0cce9920d9d883e2c2eb767c6d2fa29e6be83b3
Oleg Rekutin <orekutin@apptegic.com>
2012-12-12 16:22:40 -0500

    fix get all not working with locked entries

--- hazelcast/src/main/java/com/hazelcast/impl/concurrentmap/GetAllCallable.java
+++ hazelcast/src/main/java/com/hazelcast/impl/concurrentmap/GetAllCallable.java
@@ -94,7 +94,7 @@ public class GetAllCallable implements Callable<Pairs>, HazelcastInstanceAware, 
                         Data dValue = toData(value);
                         if (dKey != null && dValue != null) {
                             pairs.addKeyValue(new KeyValue(dKey, dValue));
-                            c.putTransient(mapName, key, value, -1);
+                            c.putFromLoad(mapName, key, value);
                         } else {
                             missingKeys.add(dKey);
                         }
